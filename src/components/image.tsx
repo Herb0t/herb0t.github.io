596265c0fb9bd7c910cebf9b83b9adec6d31b8bf
Herbert Lemus <herbert.a.lemus@gmail.com>
2024-03-28 17:47:04 -0400

    images moving

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -7,11 +7,8 @@ type ImgProps = {
 };
 
 const Img: FunctionComponent<ImgProps> = ({ className, file, extention }) => {
-  // get the value of development from the environment variable
-  const isDev = process.env.NODE_ENV === "DEVELOPMENT";
-
   // if development, return the image from the local folder
-  const imgSrc = isDev ? `./src/assets/${file}.${extention}` : `./assets/${file}.${extention}`;
+  const imgSrc = `./images/${file}.${extention}`;
 
   return (
     <div className={className} style={{backgroundImage: `url(${imgSrc})`}}>
