2fd561ee6bfb8756dc0d1a2c81255d403d06a9b9
Herbert Lemus <herbert.a.lemus@gmail.com>
2024-03-28 16:36:23 -0400

    dev vs prod

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -7,9 +7,14 @@ type ImgProps = {
 };
 
 const Img: FunctionComponent<ImgProps> = ({ className, file, extention }) => {
+  // check if development or production
+  const isDev = process.env.NODE_ENV === "development";
+  // if development, return the image from the local folder
+  const imgSrc = isDev ? `./src/assets/${file}.${extention}` : `./assets/${file}.${extention}`;
+
   return (
-    <div className={className} style={{backgroundImage: `url(/src/assets/${file}.${extention})`}}>
-      <img src={`/src/assets/${file}.${extention}`} alt="placeholder" />
+    <div className={className} style={{backgroundImage: `url(${imgSrc})`}}>
+      <img src={imgSrc} alt="placeholder" />
     </div>
   );
 };
